b4bdd001d85e637559f4ff629c429588f7a05c3f
mlksathil <mahmoodathil12@gmail.com>
2022-04-28 16:16:19 +0530

    update calculated resizing

--- sketch.js
+++ sketch.js
@@ -1,1 +1,37 @@
 // Container Grid Calculation
+
+const container = document.querySelector('.container');
+let containerWidth = (window.getComputedStyle(container).width).replace('px','');
+let containerHeight = (window.getComputedStyle(container).height).replace('px','');
+
+let rangeSlider = document.querySelector('input[type="range"]');
+
+let subGridWidth = containerWidth / rangeSlider.value;
+let subGridHeight = containerHeight / rangeSlider.value;
+
+function createElementGrid(rangeSlider,container) {
+
+    for (let i = 0; i < rangeSlider**2; i++ ) {
+        div = document.createElement('div');
+        div.classList.add('grid');
+        div.setAttribute('id',i);
+        div.style.setProperty('width', `${subGridWidth}px`);
+        div.style.setProperty('height', `${subGridHeight}px`);
+
+
+        container.appendChild(div);
+    }
+}
+
+function resizeDivs() {
+    containerWidth = (window.getComputedStyle(container).width).replace('px','');
+    containerHeight = (window.getComputedStyle(container).height).replace('px','');
+
+    subGridWidth = containerWidth / rangeSlider.value;
+    subGridHeight = containerHeight / rangeSlider.value;
+
+    div.style.setProperty('width', `${subGridWidth}px`);
+    div.style.setProperty('height', `${subGridHeight}px`);
+}
+
+document.addEventListener('DOMContentLoaded',createElementGrid(rangeSlider, container));
