d501cd27e93e21f50e7ce8012051a348affe4608
mlksathil <mahmoodathil12@gmail.com>
2022-04-28 18:17:09 +0530

    Adding in Range Slider Changes
    Adding Reset Button Changes
    
    Adding Anchors, Comments and Reviews

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let subGridWidth, subGridHeight;
 
 // Setting timeout for Window resize
 let timeout = false;
+let timeoutRange = false;
 
 function createElementGrid(rangeSliderValue,container) {
 
@@ -32,7 +33,7 @@ function resizeResetDivs(event) {
     let divs = container.children;
     containerWidth = (window.getComputedStyle(container).width).replace('px','');
     containerHeight = (window.getComputedStyle(container).height).replace('px','');
-    console.log(containerHeight, containerHeight)
+    // console.log(containerHeight, containerHeight)
 
     subGridWidth = containerWidth / rangeSlider.value;
     subGridHeight = containerHeight / rangeSlider.value;
@@ -42,15 +43,44 @@ function resizeResetDivs(event) {
         div.style.width = `${subGridWidth}px`;
         div.style.height =`${subGridHeight}px`;
         if(event.target.value === "Reset") {
-            div.style['background-color'] = 'white';
+            div.style['background-color'] = 'none';
         }
     }
 }
 
+function reArrangeDivs() {
+    // ✅ REVIEW: Using innerText to remove all inner elements
+    container.innerText = '';
+
+    containerWidth = (window.getComputedStyle(container).width).replace('px','');
+    containerHeight = (window.getComputedStyle(container).height).replace('px','');
+    // console.log(containerHeight, containerHeight)
+
+    rangeSliderValue = rangeSlider.value;
+
+    subGridWidth = containerWidth / rangeSlider.value;
+    subGridHeight = containerHeight / rangeSlider.value;
+
+    console.log(subGridHeight, subGridWidth);
+
+    for (let i = 0; i < rangeSliderValue**2; i++ ) {
+        div = document.createElement('div');
+        div.classList.add('grid');
+        div.setAttribute('id',i);
+        div.style.setProperty('width', `${subGridWidth}px`);
+        div.style.setProperty('height', `${subGridHeight}px`);
+
+        container.appendChild(div);
+    }
+}
+
+// SECTION: START
 // Compute Grids
 document.addEventListener('DOMContentLoaded',createElementGrid(rangeSlider.value, container));
 
-// Compute Grids on Resize of Window
+// SECTION: ON RESIZE & BUTTON CLICK & SLIDE CHANGE
+// // ✅ REVIEW: Learnt about Timeout
+// 1. Compute Grids on Resize of Window
 window.addEventListener('resize',function(event) {
     // clear the timeout
     clearTimeout(timeout);
@@ -58,7 +88,21 @@ window.addEventListener('resize',function(event) {
     timeout = setTimeout(resizeResetDivs(event), 100);
 });
 
-let ResetButton = document.querySelector('input[type="button"]')
 
+// 2. RESET BUTTON CLICK
+let ResetButton = document.querySelector('input[type="button"]')
 ResetButton.addEventListener('click',resizeResetDivs)
 
+// 3. RANGE SLIDE CHANGE
+// ✅ REVIEW: Adding more than 1 event as array with forEach
+let RangeSlider = document.querySelector('input[type="range"]')
+RangeSlider.addEventListener('input',function() {
+    // clear the timeout
+    clearTimeout(timeoutRange);
+    // start timing for event "completion"
+    timeoutRange = setTimeout(reArrangeDivs, 80);
+})
+
+// for (const child of container.children) {
+//     child.addEventListener('hover',)
+// }
